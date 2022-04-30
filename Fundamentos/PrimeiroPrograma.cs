e1c9e6bb07a034c5767ab0a7563b765269ab8cb1
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-04-30 15:14:37 -0300

    Adicionar arquivos de projeto.

---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class PrimeiroPrograma
+    {
+        static void Executar()
+        {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
