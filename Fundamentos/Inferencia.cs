a7bb53b6c3c9c2e2c3abc837d0f8a2e2059a2257
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-05-02 12:20:49 -0300

    Inferência de Tipos

---
+++ Fundamentos/Inferencia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class Inferencia
+    {
+        public static void Executar()
+        {
+            var nome = "Leonardo";
+
+            //nome = 123;
+            Console.WriteLine(nome);
+
+            // int idade;
+            var idade = 32;
+            Console.WriteLine(idade);
+
+            int a;
+            a = 3;
+
+            int b = 2;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace CursoCSharp {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar },
                 {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar },
+                {"Inferência - Fundamentos", Inferencia.Executar },
             });
 
             central.SelecionarEExecutar();
