af01a31e17853699bfc824c6bd5a1eedfe53f657
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-05-02 16:20:04 -0300

    Notação Ponto

---
+++ Fundamentos/NotacaoPonto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class NotacaoPonto
+    {
+        public static void Executar()
+        {
+            var saudacao = "olá".ToUpper().Insert(3, " World!").Replace("World!", "Mundo!");
+            Console.WriteLine(saudacao);
+
+            Console.WriteLine("Teste".Length);
+
+            string valorImportante = null;
+            Console.WriteLine(valorImportante?.Length);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace CursoCSharp {
                 {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar },
                 {"Inferência - Fundamentos", Inferencia.Executar },
                 {"Interpolação - Fundamentos", Interpolacao.Executar },
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar },
             });
 
             central.SelecionarEExecutar();
