4ce8672c1eb4d03fec720a07ff871d1354bb2f55
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-05-03 16:20:02 -0300

    Operadores Unários

---
+++ Fundamentos/OperadoresUnarios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class OperadoresUnarios
+    {
+        public static void Executar()
+        {
+            var valorNegativo = -5;
+            var numero1 = 2;
+            var numero2 = 3;
+            var booleano = true;
+
+            Console.WriteLine(-valorNegativo);
+            Console.WriteLine(!booleano);
+
+            numero1++;
+            Console.WriteLine(numero1);
+
+            --numero1;
+            Console.WriteLine(numero1);
+
+            Console.WriteLine(numero1++ == --numero2);
+            Console.WriteLine($"{numero1} {numero2}");
+        }
+    }
+}
