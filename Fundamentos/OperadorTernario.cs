1f531f522f09154cdd81009b73be01f76ceede90
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-05-03 16:26:29 -0300

    Operador Ternário

---
+++ Fundamentos/OperadorTernario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class OperadorTernario
+    {
+        public static void Executar()
+        {
+            var nota = 6.0;
+            bool bomComportamento = true;
+            var resultado = nota >= 7.0 && bomComportamento ? "Aprovado" : "Reprovado";
+            Console.WriteLine(resultado);
+        }
+    }
+}
