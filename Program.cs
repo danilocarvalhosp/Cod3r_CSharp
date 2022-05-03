57fe252f00f1a9e02a359b11251324d873ceffc4
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-05-03 14:20:22 -0300

    Operadores Relacionais

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace CursoCSharp {
                 {"Formatando Números - Fundamentos", FormatandoNumero.Executar },
                 {"Conversões - Fundamentos", Conversoes.Executar },
                 {"Operadores Aritméticos - Fundamentos", OperadoresAritmeticos.Executar },
+                {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar },
             });
 
             central.SelecionarEExecutar();
