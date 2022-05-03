4e912a0659806ff66fcb6f5c5273562cb0f0dff8
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-05-03 15:34:23 -0300

    Operadores Lógicos

---
+++ Fundamentos/OperadoresLogicos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    internal class OperadoresLogicos
+    {
+        public static void Executar()
+        {
+            var executouTrabalho1 = false;
+            var executouTrabalho2 = false;
+
+            bool comprouTv50 = executouTrabalho1 && executouTrabalho2;
+            Console.WriteLine("Comprou a TV 50? {0}", comprouTv50);
+
+            var comprouSorvete = executouTrabalho1 || executouTrabalho2;
+            Console.WriteLine("Comprou o sorvete? {0}", comprouSorvete);
+
+            var comprouTv32 = executouTrabalho1 ^ executouTrabalho2;
+            Console.WriteLine("Comprou a TV 32? {0}",  comprouTv32);
+
+            Console.WriteLine("Mais saudável? {0}", !comprouSorvete);
+        }
+    }
+}
