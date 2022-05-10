f2861007b69be744107238f39f98a064299ef53a
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-05-10 11:05:38 -0300

    Estrutura SWITCH

---
+++ EstruturaDeControle/EstruturaSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    internal class EstruturaSwitch
+    {
+        public static void Executar()
+        {
+            Console.Write("Avalie meu atendimento com uma nota de 1 a 5:");
+            int.TryParse(Console.ReadLine(), out int nota);
+
+            switch (nota)
+            {
+                case 0:
+                    Console.WriteLine("Péssimo");
+                        break;
+                case 1:
+                case 2:
+                    Console.WriteLine("Ruim");
+                        break;
+                case 3:
+                    Console.WriteLine("Regular");
+                        break;
+                case 4:
+                    Console.WriteLine("Bom");
+                        break;
+                case 5:
+                    Console.WriteLine("Ótimo");
+                    Console.WriteLine("Parabéns!");
+                        break;
+                default:
+                    Console.WriteLine("Nota inválida");
+                        break;
+            }
+
+            Console.WriteLine("Obrigado por responder!!!");
+        }
+    }
+}
