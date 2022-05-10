d3cdffdfaec6488df80f0a04d59c2458b2a59d1d
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-05-10 10:53:04 -0300

    Estrutura IF / ELSE / IF

---
+++ EstruturaDeControle/EstruturaIfElseIf.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    internal class EstruturaIfElseIf
+    {
+        public static void Executar()
+        {
+            Console.Write("Digite a nota do aluno: ");
+
+            string entrada = Console.ReadLine();
+            Double.TryParse(entrada, out double nota);
+
+            if (nota >= 9.0)
+            {
+                Console.WriteLine("Quadro de Honra!");
+            }
+            else if (nota >= 7 && nota < 9.0)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else if (nota >= 5.0 && nota < 7.0)
+            {
+                Console.WriteLine("Recuperação!");
+            }
+            else
+            {
+                Console.WriteLine("Te vejo na próxima...");
+            }
+
+            Console.WriteLine("Fim!!!");
+        }
+    }
+}
