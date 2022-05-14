92509122db9588dacaa4e524d758c72b9bfc5bfb
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-05-14 18:09:24 -0300

    Estrutura FOR

---
+++ EstruturaDeControle/EstruturaFor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    internal class EstruturaFor
+    {
+        public static void Executar()
+        {
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"O valor de i é {i}.");
+            //}
+
+            double somatorio = 0;
+            string entrada;
+
+            Console.Write("Informe o tamanho da turma: ");
+            entrada = Console.ReadLine();
+            int.TryParse(entrada, out int tamanhoTurma);
+
+            for(int i = 1; i <= tamanhoTurma; i++)
+            {
+                Console.Write("Informe a nota do aluno {0}: ", i);
+                entrada = Console.ReadLine();
+                double.TryParse(entrada, out double notaAtual);
+
+                somatorio += notaAtual;
+            }
+
+            double media = tamanhoTurma > 0 ? somatorio / tamanhoTurma : 0;
+            Console.Write("Média da turma: {0}", media);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -30,9 +30,10 @@ namespace CursoCSharp {
                 {"Estrutura If - Estruturas de Controle", EstruturaIf.Executar },
                 {"Estrutura If / Else - Estruturas de Controle", EstruturaIfElse.Executar },
                 {"Estrutura If / Else / If - Estruturas de Controle", EstruturaIfElseIf.Executar },
-                {"Estrutura Switch", EstruturaSwitch.Executar },
-                {"Estrutura While", EstruturaWhile.Executar },
-                {"Estrutura Do While", EstruturaDoWhile.Executar },
+                {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar },
+                {"Estrutura While - Estruturas de Controle", EstruturaWhile.Executar },
+                {"Estrutura Do While - Estruturas de Controle", EstruturaDoWhile.Executar },
+                {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar },
             });
 
             central.SelecionarEExecutar();
