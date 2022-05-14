8a35fe5af9b21cd51af94b47ecebf8407bbb6c74
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-05-14 18:15:33 -0300

    Estrutura FOREACH

---
+++ EstruturaDeControle/EstruturaForEach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    internal class EstruturaForEach
+    {
+        public static void Executar()
+        {
+            var palavra = "Opa!";
+
+            foreach (var letra in palavra)
+            {
+                Console.WriteLine(letra);
+            }
+
+            var alunos = new string[] { "Ana", "Bia", "Carlos" };
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+    }
+}
