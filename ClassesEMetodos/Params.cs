61a87f4abe27b642e15d02bab6a6e1f75856dfb3
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-05-15 18:38:27 -0300

    Parâmetros Variáveis

---
+++ ClassesEMetodos/Params.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    internal class Params
+    {
+        public static void Recepcionar(params string[] pessoas)
+        {
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine("Olá {0}", pessoa);
+            }
+        }
+        public static void Executar()
+        {
+            Recepcionar("Pedro", "Manu", "Roger", "Ana", "Bia");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -46,6 +46,7 @@ namespace CursoCSharp {
                 {"Métodos Estáticos - Classes e Métodos", MetodosEstaticos.Executar },
                 {"Atributos Estáticos - Classes e Métodos", AtributosEstaticos.Executar },
                 {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar },
+                {"Params - Classes e Métodos", Params.Executar },
             });
 
             central.SelecionarEExecutar();
