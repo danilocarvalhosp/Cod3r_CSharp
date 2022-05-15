6d6f5591383a27099d1b793c22e68d4d8e83e67b
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-05-15 17:51:57 -0300

    Métodos com Retorno #01

---
+++ ClassesEMetodos/MetodosComRetorno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class CalculadoraComum
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Dividr (int a, int b)
+        {
+            return a / b;
+        }
+    }
+
+    internal class MetodosComRetorno
+    {
+        public static void Executar()
+        {
+            var calculadoraComum = new CalculadoraComum();
+            var resultado = calculadoraComum.Somar(5, 5);
+            Console.WriteLine(resultado);
+            Console.WriteLine(calculadoraComum.Subtrair(2, 7));
+            Console.WriteLine(calculadoraComum.Multiplicar(4,4));
+            Console.WriteLine(calculadoraComum.Dividr(10, 5));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -42,6 +42,7 @@ namespace CursoCSharp {
                 // Classes e Métodos
                 {"Membros - Classes e Métodos", Membros.Executar },
                 {"Construtores - Classes e Métodos", Construtores.Executar },
+                {"Métodos com Retorno - Classes e Métodos", MetodosComRetorno.Executar },
 
             });
 
