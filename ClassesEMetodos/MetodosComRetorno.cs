7cce3914856a7b9eff3b3a5028b6e3860e249fad
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-05-15 18:02:57 -0300

    Métodos com Retorno #02

--- ClassesEMetodos/MetodosComRetorno.cs
+++ ClassesEMetodos/MetodosComRetorno.cs
@@ -29,6 +29,40 @@ namespace CursoCSharp.ClassesEMetodos
         }
     }
 
+    public class CalculadoraCadeia
+    {
+        int memoria;
+
+        public CalculadoraCadeia Somar(int a)
+        {
+            memoria += a;
+            return this;
+        }
+
+        public CalculadoraCadeia Multiplicar(int a)
+        {
+            memoria *= a;
+            return this;
+        }
+
+        public CalculadoraCadeia Limpar()
+        {
+            memoria = 0;
+            return this;
+        }
+
+        public int Resultado()
+        {
+            return memoria;
+        }
+
+        public CalculadoraCadeia Imprimir()
+        {
+            Console.WriteLine(memoria);
+            return this;
+        }
+    }
+
     internal class MetodosComRetorno
     {
         public static void Executar()
@@ -37,8 +71,14 @@ namespace CursoCSharp.ClassesEMetodos
             var resultado = calculadoraComum.Somar(5, 5);
             Console.WriteLine(resultado);
             Console.WriteLine(calculadoraComum.Subtrair(2, 7));
-            Console.WriteLine(calculadoraComum.Multiplicar(4,4));
+            Console.WriteLine(calculadoraComum.Multiplicar(4, 4));
             Console.WriteLine(calculadoraComum.Dividr(10, 5));
+
+            var calculadoraCadeia = new CalculadoraCadeia();
+            calculadoraCadeia.Somar(3).Multiplicar(3).Imprimir().Limpar().Imprimir();
+
+            resultado = calculadoraCadeia.Somar(3).Multiplicar(2).Resultado();
+            Console.WriteLine(resultado);
         }
     }
 }
