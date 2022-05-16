2c0ba3c8a1fd631717fdfff2f79cc27ea1ffbe18
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-05-16 19:53:47 -0300

    Getters & Setters

---
+++ ClassesEMetodos/GetSet.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class Moto
+    {
+        private string Marca;
+        private string Modelo;
+        private uint Cilindrada;
+
+        public Moto(string marca, string modelo, uint cilindrada)
+        {
+            //Marca = marca;
+            //Modelo = modelo;
+            //Cilindrada = cilindrada;
+            SetMarca(marca);
+            SetModelo(modelo);
+            SetCilindrada(cilindrada);
+        }
+
+        public Moto() { }
+
+        public string GetMarca()
+        {
+            return Marca;
+        }
+
+        public void SetMarca(string marca)
+        {
+            Marca = marca;
+        }
+
+        public string GetModelo()
+        {
+            return Modelo;
+        }
+
+        public void SetModelo(string modelo)
+        {
+            Modelo = modelo;
+        }
+
+        public uint GetCilindrada()
+        {
+            return Cilindrada;
+        }
+
+        public void SetCilindrada(uint cilindrada)
+        {
+            // 1ª OPÇÃO
+            //if(cilindrada > 0)
+            //{
+            //    Cilindrada = cilindrada;
+            //}
+            
+            // 2ª OPÇÃO
+            //Cilindrada = Math.Abs(cilindrada);
+
+            // 3ª OPÇÃO
+            Cilindrada = cilindrada;
+        }
+    }
+
+    internal class GetSet
+    {
+        public static void Executar()
+        {
+            var moto1 = new Moto("Kawasaki", "Ninja ZX-6R", 636);
+            Console.WriteLine(moto1.GetMarca());
+            Console.WriteLine(moto1.GetModelo());
+            Console.WriteLine(moto1.GetCilindrada());
+
+            var moto2 = new Moto();
+            moto2.SetMarca("Honda");
+            moto2.SetModelo("CG Titan");
+            moto2.SetCilindrada(150);
+            Console.WriteLine(moto2.GetMarca() + " " + moto2.GetModelo() + " " + moto2.GetCilindrada());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -48,6 +48,7 @@ namespace CursoCSharp {
                 {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar },
                 {"Params - Classes e Métodos", Params.Executar },
                 {"Parâmetros Nomeados - Classes e Métodos", ParametrosNomeados.Executar },
+                {"Get e Set - Classes e Métodos", GetSet.Executar },
             });
 
             central.SelecionarEExecutar();
