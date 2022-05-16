db9af7a129d915de55217f03e94cdfc14e1844c8
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-05-16 20:09:44 -0300

    Propriedades

---
+++ ClassesEMetodos/Props.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class CarroOpcional
+    {
+        double desconto = 0.1;
+
+        string nome;
+        public string Nome
+        {
+            get { return "Opcional: " + nome; }
+            set { nome = value; }
+        }
+
+        // Propriedade autoimplementada
+        public double Preco { get; set; }
+
+        // Somente leitura
+        public double PrecoComDesconto
+        {
+            // get => Preco - (desconto * Preco);
+            get { return Preco - (desconto * Preco); }
+        }
+
+        public CarroOpcional() { }
+
+        public CarroOpcional(string nome, double preco)
+        {
+            this.nome = nome;
+            Preco = preco;
+        }
+    }
+
+    internal class Props
+    {
+        public static void Executar()
+        {
+            var op1 = new CarroOpcional("Ar condicionado", 3499.90);
+            Console.WriteLine(op1.PrecoComDesconto);
+
+            // op1.PrecoComDesconto = 3000;
+
+            var op2 = new CarroOpcional();
+            op2.Nome = "Direção Elétrica";
+            op2.Preco = 2349.90;
+
+            Console.WriteLine(op1.Nome);
+            Console.WriteLine(op1.Preco);
+
+            Console.WriteLine(op2.Nome);
+            Console.WriteLine(op2.Preco);
+            Console.WriteLine(op2.PrecoComDesconto);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -49,6 +49,7 @@ namespace CursoCSharp {
                 {"Params - Classes e Métodos", Params.Executar },
                 {"Parâmetros Nomeados - Classes e Métodos", ParametrosNomeados.Executar },
                 {"Get e Set - Classes e Métodos", GetSet.Executar },
+                {"Props - Classes e Métodos", Props.Executar },
             });
 
             central.SelecionarEExecutar();
