75c3ce84213a5ec9e257baf75b6d8d299c2f5a7e
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-05-21 11:22:10 -0300

    Atributos READONLY

---
+++ ClassesEMetodos/ReadOnly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class Cliente
+    {
+        public string Nome;
+        readonly DateTime Nascimento;
+
+        public Cliente(string nome, DateTime nascimento)
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+        }
+
+        public string GetDataDeNascimento()
+        {
+            return String.Format("{0}/{1}/{2}", Nascimento.Date, Nascimento.Month, Nascimento.Year);
+        }
+    }
+    internal class ReadOnly
+    {
+        public static void Executar()
+        {
+            var novoCliente = new Cliente("Ana Silva", new DateTime(1987, 5, 22));
+
+            Console.WriteLine(novoCliente.Nome);
+            Console.WriteLine(novoCliente.GetDataDeNascimento());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -50,6 +50,7 @@ namespace CursoCSharp {
                 {"Parâmetros Nomeados - Classes e Métodos", ParametrosNomeados.Executar },
                 {"Get e Set - Classes e Métodos", GetSet.Executar },
                 {"Props - Classes e Métodos", Props.Executar },
+                {"Read Only - Classes e Métodos", ReadOnly.Executar },
             });
 
             central.SelecionarEExecutar();
