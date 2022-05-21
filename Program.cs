3d955255a201d8c2acacdeb30e08624b4e911fac
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-05-21 12:14:57 -0300

    Atribuição por Valor vs Referência

--- Program.cs
+++ Program.cs
@@ -54,6 +54,7 @@ namespace CursoCSharp {
                 {"Enum - Classes e Métodos", ExemploEnum.Executar },
                 {"Struct - Classes e Métodos", ExemploStruct.Executar },
                 {"Struct Vs Classe - Classes e Métodos", StructVsClasse.Executar },
+                {"Valor Vs Referência - Classes e Métodos", ValorVsReferencia.Executar },
             });
 
             central.SelecionarEExecutar();
