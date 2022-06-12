52587a66b00d00dae236f317abfc8e694c9cafff
Danilo Carvalho <danilocarvalhosp@gmail.com>
2022-06-12 18:38:34 -0300

    Dictionary

--- Program.cs
+++ Program.cs
@@ -67,6 +67,7 @@ namespace CursoCSharp {
                 {"Queue - Coleções", ColecoesQueue.Executar },
                 {"Igualdade - Coleções", Igualdade.Executar },
                 {"Stack - Coleções", ColecoesStack.Executar },
+                {"Dictionary - Coleções", ColecoesDictionary.Executar },
             });
 
             central.SelecionarEExecutar();
