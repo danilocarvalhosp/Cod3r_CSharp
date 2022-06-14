4d892c0b88ae6c85f5584eaa6c865e34c40fb22a
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-06-14 15:56:57 -0300

    Encapsulamento

--- Program.cs
+++ Program.cs
@@ -73,6 +73,7 @@ namespace CursoCSharp {
                 // OO - Orientação a Objetos
                 {"Herança - OO", Heranca.Executar },
                 {"Construtor This - OO", ConstrutorThis.Executar },
+                {"Encapsulamento - OO", OO.Encapsulamento.Executar },
             });
 
             central.SelecionarEExecutar();
