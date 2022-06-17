ef8a4f2eb21f9be5cc57240483f08bfbfc4385e6
danilocarvalhosp <danilocarvalhosp@gmail.com>
2022-06-17 12:08:03 -0300

    Encapsulamento

---
+++ Encapsulamento/Encapsulamento/SubCelebridade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    public class SubCelebridade
+    {
+        // ACESSADO POR TODOS
+        public string InfoPublica = "Tenho um instagram!";
+
+        // ACESSADO POR HERANÇA
+        protected string CorDoOlho = "Verde";
+
+        // ACESSADO NO MESMO PROJETO (MESMO ASSBEMLY)
+        internal ulong NumeroCelular = 5511999999999;
+
+        // ACESSADO POR HERANÇA OU NO MESMO PROJETO (MESMO ASSEMBLY)
+        protected internal string JeitoDeFalar = "Uso muitas gírias";
+
+        // ACESSADO NA MESMA CLASSE OU POR HERANÇA NO MESMO PROJETO
+        private protected string SegredoFamilia = "Bla bla";
+
+        // ACESSADO SOMENTE NA MESMA CLASSE
+        bool UsaMuitoPhotoshop = true;
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("SubCelebridade...");
+
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(SegredoFamilia);
+            Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
+}
